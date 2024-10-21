4e9396b2a71e0bbf42741553c9a26fc54a1a0409
hazvyg <hgwembe@gmail.com>
2024-10-21 15:20:21 +0200

    added some javascrpit for functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-alert("Helo World!!");
+function generatePoem(event) {
+    event.preventDefault();
+
+    new Typewriter("#poem", {
+      strings: ["lorem ipsum tada lalalalalallala"],
+        autoStart: true,
+        delay: 1,
+      cursor:"",
+    });
+
+    
+
+}
+let poemFormElement = document.querySelector("#poem-generator");
+poemFormElement.addEventListener("submit", generatePoem);
